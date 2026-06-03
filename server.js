f2e7239bef806a8eada08af49edb426b2854283e
AngelRizo061007 <ahernandez212@ucol.mx>
2026-06-02 18:33:24 -0600

    Corregir carga de archivos CSS y JS

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 
 // Esto servirá los archivos desde la misma carpeta donde está el server.js
-app.use(express.static(__dirname)); 
+app.use(express.static(__dirname)); // Sirve todo lo que esté en la carpeta raíz 
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
