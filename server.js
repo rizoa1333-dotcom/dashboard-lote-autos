a1df037bb1de0dfd832f7340cdc643a8a276f14b
AngelRizo061007 <ahernandez212@ucol.mx>
2026-06-02 18:10:32 -0600

    Corrigiendo ruta de archivos en server.js

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static('public')); // Mueve tu index.html, style.css y script.js a una carpeta llamada 'public'
+// Esto servirá los archivos desde la misma carpeta donde está el server.js
+app.use(express.static(__dirname)); 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
+app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
